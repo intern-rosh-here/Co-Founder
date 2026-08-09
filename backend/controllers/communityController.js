7479c71424de0a9c46b385bfb9cc4d801d2c5a34
Roshan-B-ctrl <roshanbright07@gmail.com>
2026-08-09 19:54:21 +0530

    Fix community post Cloudinary media URL

--- backend/controllers/communityController.js
+++ backend/controllers/communityController.js
@@ -72,7 +72,7 @@ exports.createPost = async (req, res) => {
 
 const media =
   req.files?.map((file) => ({
-    url: file.path,
+    url: file.path || file.secure_url,
     fileName: file.originalname,
     type: file.mimetype.startsWith('image')
       ? 'image'
